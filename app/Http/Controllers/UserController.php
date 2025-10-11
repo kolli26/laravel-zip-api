50cbcd9be5aed895d0f083ff3637c7c7628e912c
kolli26 <kollarandras2006@gmail.com>
2025-10-11 16:57:58 +0200

    worked on user endpoint

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = User::where('email', $email)->first();
+
+        if (!$user || !Hash::check($password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ]);
+        }
+
+        $user->tokens()->delete();
+
+        $user->token = $user->createToken('access')->plainTextToken;
+
+        return response()->json([
+            'data' => $user,
+        ]);
+    }
+}
